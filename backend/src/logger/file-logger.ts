c2c821c68f140667448ef3295192ea2ca8fb9da9
Nelkor <tk.nelkor@gmail.com>
2021-03-12 10:04:30 +0300

    date fns to utc

--- backend/src/logger/file-logger.ts
+++ backend/src/logger/file-logger.ts
@@ -4,9 +4,9 @@ const getDashedYmd = () => {
   const date = new Date()
 
   return [
-    date.getFullYear(),
-    date.getMonth() + 1,
-    date.getDate(),
+    date.getUTCFullYear(),
+    date.getUTCMonth() + 1,
+    date.getUTCDate(),
   ]
     .map(item => item.toString().padStart(2, '0'))
     .join('-')
